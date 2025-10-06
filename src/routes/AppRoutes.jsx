b24d7c27e8f0a89800d4708c09fc36e03dc554b3
beatrizveloso <beatrizoslov6@gmail.com>
2025-10-06 14:06:35 -0300

    correção de erros2

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"; 
-import HomePage from "../pages/homepage/HomePage.jsx"; 
-import MarryKiss from "../pages/MarryKiss/MarryKiss.jsx";
-import ScrollToTop from "./ScrollToTop";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/homepage/HomePage.jsx";
+import MarryKiss from "../pages/marrykiss/MarryKiss.jsx";
 
-const AppRoutes = () => { 
+const AppRoutes = () => {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/MarryKiss" element={<MarryKiss />} />
-        <Route path="*" element={<h1>Not Found</h1>} />
+        <Route path="*" element={<h1>Página Não Encontrada</h1>} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
